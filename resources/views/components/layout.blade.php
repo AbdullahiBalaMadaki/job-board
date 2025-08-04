52ef4730fef54c3dfb08068fe7f1a97c53bfafb3
A. B. Madaki <madakisoft@fmail.com>
2025-08-04 15:40:13 +0100

    Jobs Page and Card Component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,1 +1,12 @@
-<h1 class="text-red-500 text-4xl">Hello Tailwind</h1>
+<! DOCTYPE html>
+<html lang="{f str_replace('-', '-', app(→getLocale()) 33">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel Job Board</title>
+    @vite( 'resources/css/app.css')
+</head>
+<body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+    {{ $slot }}
+</body>
+</html>
