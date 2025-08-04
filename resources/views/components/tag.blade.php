1983ec2809db72793385c5d5bd5866516eac2053
A. B. Madaki <madakisoft@fmail.com>
2025-08-04 15:54:08 +0100

    Jobs Page: Tag Component & Job Info

---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+</div><div class="rounded-md border px-2 py-1">
+  {{ $slot }}
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
+Route::get('', fn() => to_route('jobs.index'));
 Route::resource('jobs', JobController::class)->only(['index']);
