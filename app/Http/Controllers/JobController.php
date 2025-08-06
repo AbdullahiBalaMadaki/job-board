6571f5ac419439688d32966e0089ae2d83576572
A. B. Madaki <madakisoft@fmail.com>
2025-08-06 18:20:06 +0100

    Filtering Jobs: Min & Max Salary

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -12,12 +12,19 @@ class JobController extends Controller
      */
     public function index()
     {
-        $jobs = Job::query();
-
-        $jobs->when(request('search'), function ($query) {
-            $query->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('description', 'like', '%' . request('search') . '%');
-        });
+        $jobs = Job::query()
+            ->when(request('search'), function ($query) {
+                $query->where(function ($q) {
+                    $q->where('title', 'like', '%' . request('search') . '%')
+                      ->orWhere('description', 'like', '%' . request('search') . '%');
+                });
+            })
+            ->when(request('min_salary'), function ($query) {
+                $query->where('salary', '>=', request('min_salary'));
+            })
+            ->when(request('max_salary'), function ($query) {
+                $query->where('salary', '<=', request('max_salary'));
+            });
 
         return view('job.index', ['jobs' => $jobs->get()]);
     }
@@ -43,7 +50,7 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
-       return view('job.show', compact('job'));
+        return view('job.show', compact('job'));
     }
 
     /**
