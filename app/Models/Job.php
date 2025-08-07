978d7aff78f55d1285c07412ad902f8822cb2400
A. B. Madaki <madakisoft@fmail.com>
2025-08-07 21:18:59 +0100

    Employer: Model, Migration, Relations

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Job extends Model
@@ -18,11 +19,16 @@ class Job extends Model
         'Sales',
         'Marketing'
     ];
-public function scopeFilter(Builder|QueryBuilder $query, array $filters): Builder|QueryBuilder
+
+    public function employer(): BelongsTo
     {
-        return $query->when($filters['search'] ?? null, function ($query, $search) {
-            $query->where(function ($query) use ($search) {
-                $query->where('title', 'like', '%' . $search . '%')
+        return $this->belongsTo(Employer::class);
+    }
+    public function scopeFilter(Builder|QueryBuilder $query, array $filters): Builder|QueryBuilder
+        {
+            return $query->when($filters['search'] ?? null, function ($query, $search) {
+                $query->where(function ($query) use ($search) {
+                    $query->where('title', 'like', '%' . $search . '%')
                     ->orWhere('description', 'like', '%' . $search . '%');
             });
         })->when($filters['min_salary'] ?? null, function ($query, $minSalary) {
