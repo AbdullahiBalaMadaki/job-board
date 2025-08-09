9d8a41e29e154d901cc05dcb5de6cf60327c54f5
A. B. Madaki <madakisoft@fmail.com>
2025-08-09 16:42:25 +0100

    Employer: Middleware Checking for Permissions

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\MyJobController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\MyJobApplicationController;
@@ -26,4 +27,7 @@ Route::middleware('auth')->group(function () {
 
         Route::resource('employer', EmployerController::class)
         ->only(['create', 'store']);
+
+        Route::middleware('employer')
+        ->resource('my-jobs', MyJobController::class);
 });
