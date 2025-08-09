33dd7f248ccd5aff42aed8e20e62ffa979507e8a
A. B. Madaki <madakisoft@fmail.com>
2025-08-09 12:35:34 +0100

    My Applications: Cancelling Job Applications

--- app/Http/Controllers/MyJobApplicationController.php
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\JobApplication;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
@@ -28,8 +28,13 @@ class MyJobApplicationController extends Controller
         );
     }
 
-    public function destroy(string $id)
+    public function destroy(JobApplication $myJobApplication)
     {
-        //
+        $myJobApplication->delete();
+
+        return redirect()->back()->with(
+            'success',
+            'Job application removed.'
+        );
     }
 }
